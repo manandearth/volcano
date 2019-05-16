1923e7f692550e29c2d2c665fd3057d382d4a07b
manandearth <adamgefen@posteo.net>
2019-05-16 11:27:05 +0200

    Add the json data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,141 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Volcano from './volcano.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillMount(){
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+    
+    render() {
+        return (
+            <div>
+              <h1>It is {this.state.date.toLocaleTimeString()}</h1>
+            </div>
+        );
+    }
+}
+
+class Toggle extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn :true};
+
+         this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(state =>({
+            isToggleOn: !state.isToggleOn}));
+    }
+
+    render() {
+        return (
+            <div>
+              <button onClick={this.handleClick}>
+              {this.state.isToggleOn ? 'ON' : 'OFF'}
+              </button>
+             
+            <div>{this.state.isToggleOn ? 'right...' : null}</div>
+              <div>{this.state.isToggleOn ?  
+                  <Clock /> : null }
+                </div>
+            </div>
+        );
+    }
+}
+
+
+// function GetData(props) {
+//     const dataSet = Volcano.features;
+//     // const data = JSON.parse(dataSet['type']);
+//     return ) ;
+// }
+
+class App extends React.Component  {
+    render() {
+        return (
+            <div>
+            <h1>Reactive Volcanoes</h1>
+            <VolcanoNameList />
+            </div>
+        );
+    }
+}
+
+
+function VolcanoList(props) {
+    
+    const jsonList = Volcano.features;
+    
+    const jsonNames = jsonList.map((volcano) =>
+        <tr>
+          <td>{volcano['properties']['V_Name']}</td>
+          <td>{volcano['properties']['Country']}</td>
+          <td>{volcano['properties']['Latitude']}</td>
+          <td>{volcano['properties']['Longitude']}</td>
+          <td>{volcano['properties']['H_active']}</td>
+          <td>{volcano['properties']['hazard']}</td>
+          
+        </tr>
+    );
+    return jsonNames;
+}
+
+
+class VolcanoNameList extends React.Component {
+
+    
+    render() {
+        return (
+            <div>
+              <table>
+                <tr>
+                  <th>name</th>
+                  <th>Country</th>
+                  <th>Latitude</th>
+                  <th>Longitude</th>
+                  <th>Active?</th>
+                  <th>Hazard Rate</th>
+            </tr>
+                <VolcanoList />
+              </table>
+              <div>
+                {/* <GetData /> */}
+              </div>
+            </div>);
+    }
+    
+    
+}
+
+
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
