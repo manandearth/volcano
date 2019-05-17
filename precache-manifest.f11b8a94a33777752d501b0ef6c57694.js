56d91dbedd3fcf752a2432b67c2ac147bfb82b24
manandearth <adamgefen@posteo.net>
2019-05-18 00:32:06 +0200

    Updates

---
+++ precache-manifest.f11b8a94a33777752d501b0ef6c57694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ecb2183908f079af6d3f4b2071d6c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "922f8a32ac2efd2cf4f5",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a4bfaf8c4bccdd0a2cf9",
+    "url": "/static/js/2.0771dcc1.chunk.js"
+  },
+  {
+    "revision": "922f8a32ac2efd2cf4f5",
+    "url": "/static/js/main.6ecd462f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6117817c49c1bb22321c826ecbc52458",
+    "url": "/static/media/react-native-icon.6117817c.svg"
+  }
+]);
